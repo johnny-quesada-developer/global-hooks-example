63fe3411f94a87f66bb5b58df56058b4114be794
johnny quesada <johnny-qc@hotmail.com>
2025-03-06 00:48:41 +0100

    updates example

--- src/GlobalHooksExample/GlobalHooksExample.tsx
+++ src/GlobalHooksExample/GlobalHooksExample.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import merge from 'easy-css-merge';
 import styles from './GlobalHooksExample.module.scss';
 import { createGlobalState } from 'react-global-state-hooks/createGlobalState';
+import { generateStackHash } from 'react-global-state-hooks';
 
 const Title: React.FC<React.PropsWithChildren<React.HTMLAttributes<HTMLElement>>> = ({
   className = '',
@@ -302,6 +303,392 @@ const reusingSelectorsExample = (() => {
   );
 })();
 
+const listeningToStateChanges = (() => {
+  const useProgress = createGlobalState(0);
+  const [progressRetriever, progressMutator] = useProgress.stateControls();
+
+  const ComponentA = () => {
+    const [isPaused, setIsPaused] = useState(false);
+
+    useEffect(() => {
+      if (isPaused) return;
+
+      const interval = setInterval(() => {
+        progressMutator((prev) => (prev + 1) % 101);
+      }, 100);
+
+      return () => clearInterval(interval);
+    }, [isPaused]);
+
+    return (
+      <Container
+        className="flex justify-between items-center flex-wrap"
+        style={{ gridColumnStart: 'auto 1fr' }}
+      >
+        <p>You can listen to the state changes outside hooks</p>
+        <Button onClick={() => setIsPaused((prev) => !prev)}>{isPaused ? 'Resume' : 'Pause'}</Button>
+      </Container>
+    );
+  };
+
+  const ComponentB = () => {
+    const ref = useRef<HTMLProgressElement>(null);
+
+    useEffect(() => {
+      const progressElement = ref.current!;
+
+      // returns a function to unsubscribe
+      return progressRetriever((progress) => {
+        progressElement.value = progress;
+      });
+    }, []);
+
+    return (
+      <Container className="grid grid-cols-2 gap-6 auto-rows-min items-start">
+        <label className="col-span-2">Progress</label>
+
+        <progress
+          ref={ref}
+          value={0}
+          max={100}
+          style={{ viewTransitionName: 'progress' }}
+          className={merge(
+            'col-span-2 w-full h-4 appearance-none border-emphasis-primary border rounded-md overflow-hidden',
+            'bg-white [&::-webkit-progress-bar]:bg-white',
+            '[&::-webkit-progress-value]:bg-emphasis-interactive [&::-moz-progress-bar]:bg-emphasis-interactive'
+          )}
+        />
+
+        <CodeBlock className="col-span-2">
+          <pre className="text-xs">{`const ref = useRef<HTMLProgressElement>(null);`}</pre>
+          <br />
+          <pre className="text-xs">{`useEffect(() => {`}</pre>
+          <pre className="text-xs">{`  const progressElement = ref.current!;`}</pre>
+          <br />
+          <pre className="text-xs text-emphasis-primary">{`  // returns a function to unsubscribe`}</pre>
+          <pre className="text-xs text-emphasis-interactive">{`  return progressRetriever((progress) => {`}</pre>
+          <pre className="text-xs">{`    progressElement.value = progress;`}</pre>
+          <pre className="text-xs">{`  });`}</pre>
+          <pre className="text-xs">{`}, []);`}</pre>
+          <br />
+          <pre className="text-xs">{`return <progress ref={ref} value={0} max={100} />;`}</pre>
+        </CodeBlock>
+
+        <label className="text-xs col-span-2">
+          You can also simple retrieve the state value without subscribing to changes
+        </label>
+
+        <CodeBlock className="col-span-2">
+          <pre className="text-xs text-emphasis-interactive">{`console.log(progressRetriever()); // 0`}</pre>
+          <pre className="text-xs text-emphasis-primary">{`// synchronously returns the current value of the state`}</pre>
+        </CodeBlock>
+      </Container>
+    );
+  };
+
+  return (
+    <>
+      <Container className="flex flex-col gap-4">
+        <SubTitle className="">Retrieve hook controls outside of components</SubTitle>
+
+        <CodeBlock>
+          <pre className="text-xs">{`const useProgress = createGlobalState(0);`}</pre>
+          <br />
+          <pre className="text-xs text-emphasis-primary">{`// You can retrieve the state controls outside of the components or hooks`}</pre>
+          <pre className="text-xs text-emphasis-interactive">{`const [progressRetriever, progressMutator] = useProgress.stateControls();`}</pre>
+          <br />
+          <pre className="text-xs">{`const ComponentA = () => {`}</pre>
+          <pre className="text-xs text-emphasis-interactive">{`  const [isPaused, setIsPaused] = useState(true);`}</pre>
+          <br />
+          <pre className="text-xs">{`  useEffect(() => {`}</pre>
+          <pre className="text-xs">{`    if (isPaused) return;`}</pre>
+          <br />
+          <pre className="text-xs">{`    const interval = setInterval(() => {`}</pre>
+          <pre className="text-xs text-emphasis-interactive">{`      progressMutator((prev) => (prev + 1) % 101);`}</pre>
+          <pre className="text-xs">{`    }, 1000);`}</pre>
+          <br />
+          <pre className="text-xs">{`    return () => clearInterval(interval);`}</pre>
+          <pre className="text-xs">{`  }, [isPaused]);`}</pre>
+          <br />
+          <pre className="text-xs">{`...`}</pre>
+          <pre className="text-xs">{`};`}</pre>
+        </CodeBlock>
+
+        <ComponentA />
+      </Container>
+
+      <Container className="flex flex-col gap-4">
+        <SubTitle className="">Subscribe to state changes without a hooks</SubTitle>
+        <ComponentB />
+      </Container>
+    </>
+  );
+})();
+
+const moreListeningToStateChanges = (() => {
+  const useContacts = createGlobalState(getContactsMock());
+  const [contactsRetriever, setContacts] = useContacts.stateControls();
+
+  const useContactsArray = useContacts.createSelectorHook((contacts) => {
+    return [...contacts.values()];
+  });
+
+  const useSelectedContactId = createGlobalState(null as string | null, {
+    callbacks: {
+      onInit: ({ setState, getState }) => {
+        contactsRetriever((contacts) => {
+          if (contacts.has(getState()!)) return;
+
+          setState(null);
+        });
+      },
+    },
+  });
+
+  const ComponentA = () => {
+    const [contacts] = useContactsArray();
+    const [selectedContactId, setSelectedContactId] = useSelectedContactId();
+
+    return (
+      <ul className="border border-emphasis-primary rounded-md">
+        {contacts.map((contact, index) => (
+          <li
+            key={contact.id}
+            className={merge(
+              'cursor-pointer bg-opacity-10',
+              {
+                'bg-emphasis-primary': index % 2 === 0,
+                'bg-emphasis-secondary': index % 2 !== 0,
+                '!bg-emphasis-primary !opacity-100 text-white': selectedContactId === contact.id,
+              },
+              'hover:bg-opacity-50 hover:text-white hover:bg-emphasis-primary'
+            )}
+          >
+            <button
+              className="inset-0 w-full h-full p-2"
+              onClick={() => {
+                setSelectedContactId(contact.id);
+              }}
+            >
+              {contact.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
+  const ComponentB = () => {
+    const [selectedContactId] = useSelectedContactId();
+
+    return (
+      <Container className="flex gap-4 items-center flex-wrap" style={{ gridColumnStart: 'auto 1fr' }}>
+        <label>
+          <strong>Selected contact Id</strong>: {selectedContactId ?? 'None'}
+        </label>
+
+        <Button
+          onClick={() => {
+            if (!selectedContactId) return;
+
+            setContacts((contacts) => {
+              contacts.delete(selectedContactId);
+              return new Map(contacts);
+            });
+          }}
+        >
+          Delete Selected Contact
+        </Button>
+
+        <Button
+          onClick={() => {
+            setContacts(getContactsMock());
+          }}
+        >
+          Reset Example
+        </Button>
+      </Container>
+    );
+  };
+
+  return (
+    <Container className="grid grid-cols-2 gap-4 gap-x-6 auto-rows-min items-start">
+      <SubTitle className="col-span-2">Hooks who depend other hooks</SubTitle>
+
+      <label className="col-span-2">
+        Been able to listen to the state changes without a hook is very useful, let's say that you have and
+        state that depends on another one.
+      </label>
+
+      <label className="font-semibold">Component A</label>
+      <label className="font-semibold">Component B</label>
+
+      <ComponentA />
+      <ComponentB />
+
+      <label className="col-span-2 font-bold">useContacts</label>
+      <CodeBlock className="col-span-2">
+        <pre className="text-xs">{`const useContacts = createGlobalState(getContactsMock());`}</pre>
+        <br />
+        <pre className="text-xs text-emphasis-primary">{`// the retriever do both, get the state or subscribe to changes`}</pre>
+        <pre className="text-xs text-emphasis-interactive">{`const [contactsRetriever, setContacts] = useContacts.stateControls();`}</pre>
+        <br />
+        <pre className="text-xs">{`const useContactsArray = useContacts.createSelectorHook((contacts) => {`}</pre>
+        <pre className="text-xs">{`  return [...contacts.values()];`}</pre>
+        <pre className="text-xs">{`});`}</pre>
+      </CodeBlock>
+
+      <label className="col-span-2 font-bold">useSelectedContact</label>
+
+      <CodeBlock className="col-span-2">
+        <pre className="text-xs text-emphasis-primary">{`// The second argument is a configuration object, we'll analyze it from now on`}</pre>
+        <pre className="text-xs">{`const useSelectedContactId = createGlobalState(null as string | null, {`}</pre>
+        <br />
+        <pre className="text-xs text-emphasis-primary">{`// Callbacks are the lifecycle events of the state`}</pre>
+        <pre className="text-xs text-emphasis-interactive">{`  callbacks: {`}</pre>
+        <pre className="text-xs text-emphasis-interactive">{`    onInit: ({ setState, getState }) => {`}</pre>
+        <pre className="text-xs text-emphasis-primary font-semibold">{`      // If the contacts map stops containing the selected one, reset`}</pre>
+        <pre className="text-xs text-emphasis-interactive">{`      contactsRetriever((contacts) => {`}</pre>
+        <pre className="text-xs">{`        if (contacts.has(getState()!)) return;`}</pre>
+        <br />
+        <pre className="text-xs">{`        setState(null);`}</pre>
+        <pre className="text-xs">{`      });`}</pre>
+        <pre className="text-xs">{`    },`}</pre>
+        <pre className="text-xs">{`  },`}</pre>
+        <pre className="text-xs">{`});`}</pre>
+      </CodeBlock>
+    </Container>
+  );
+})();
+
+const persistStateExample = (() => {
+  const useCounter = createGlobalState(0, {
+    localStorage: {
+      key: '_counter_from_react_hooks_global_states_example',
+    },
+  });
+
+  const useCounterWithActions = createGlobalState(0, {
+    actions: {
+      // the first function defines the action name and arguments
+      increment: (value = 1) => {
+        // the return of the second function is the action itself
+        // and defines the return type of the action
+        return ({ setState, getState }): number => {
+          setState((prev) => prev + value || 1);
+
+          return getState();
+        };
+      },
+
+      decrement: (value = 1) => {
+        return ({ setState }) => {
+          setState((prev) => prev - value || 1);
+        };
+      },
+    },
+  });
+
+  const ComponentA = () => {
+    const [count, setCount] = useCounter();
+
+    return (
+      <Container className="flex gap-4 items-center flex-wrap" style={{ gridColumnStart: 'auto 1fr' }}>
+        <Button onClick={() => setCount((prev) => prev + 1)}>Increment</Button>
+        <Button onClick={() => setCount((prev) => prev - 1)}>Decrement</Button>
+        <label className="flex-1 text-right">{count}</label>
+      </Container>
+    );
+  };
+
+  const ComponentB = () => {
+    const [count, { increment, decrement }] = useCounterWithActions();
+
+    return (
+      <Container className="flex gap-4 items-center flex-wrap" style={{ gridColumnStart: 'auto 1fr' }}>
+        <Button onClick={() => increment(count)}>Increment</Button>
+        <Button onClick={() => decrement(count)}>Decrement</Button>
+        <label className="flex-1 text-right">{count}</label>
+      </Container>
+    );
+  };
+
+  return (
+    <Container className="flex flex-col gap-4">
+      <SubTitle className="">Persist state in localStorage</SubTitle>
+
+      <p>
+        If you are using{' '}
+        <a
+          className="text-emphasis-interactive hover:underline"
+          href="https://www.npmjs.com/package/react-global-state-hooks"
+        >
+          react-global-state-hooks
+        </a>
+        , which is the version of the library focused on browser usage, you can persist the state in the
+        localStorage.
+      </p>
+
+      <ComponentA />
+
+      <CodeBlock>
+        <pre className="text-xs">{`const useCounter = createGlobalState(0, {`}</pre>
+        <pre className="text-xs">{`  localStorage: {`}</pre>
+        <pre className="text-xs text-emphasis-interactive">{`    key: '_counter_from_react_hooks_global_states_example',`}</pre>
+        <pre className="text-xs">{`  },`}</pre>
+        <pre className="text-xs">{`});`}</pre>
+      </CodeBlock>
+
+      <SubTitle className="">Custom actions</SubTitle>
+      <p>You can also restrict the manipulation of the state to and specific set of actions.</p>
+
+      <CodeBlock className="col-span-2">
+        <pre className="text-xs">{`const useCounterWithActions = createGlobalState(0, {`}</pre>
+        <pre className="text-xs text-emphasis-interactive">{`  actions: {`}</pre>
+        <pre className="text-xs text-emphasis-primary">{`    // the first function defines the action name and arguments`}</pre>
+        <pre className="text-xs  text-emphasis-interactive">{`    increment: (value = 1) => {`}</pre>
+        <pre className="text-xs text-emphasis-primary">{`      // the return of the second function is the action itself`}</pre>
+        <pre className="text-xs text-emphasis-primary">{`      // and defines the return type of the action`}</pre>
+        <pre className="text-xs  text-emphasis-interactive">{`      return ({ setState, getState }): number => {`}</pre>
+        <pre className="text-xs">{`        setState((prev) => prev + value || 1);`}</pre>
+        <br />
+        <pre className="text-xs">{`        return getState();`}</pre>
+        <pre className="text-xs">{`      };`}</pre>
+        <pre className="text-xs">{`    },`}</pre>
+        <pre className="text-xs text-emphasis-interactive">{`    decrement: (value = 1) => {`}</pre>
+        <pre className="text-xs text-emphasis-interactive">{`      return ({ setState }) => {`}</pre>
+        <pre className="text-xs">{`        setState((prev) => prev - value || 1);`}</pre>
+        <br />
+        <pre className="text-xs">{`        return getState();`}</pre>
+        <pre className="text-xs">{`      };`}</pre>
+        <pre className="text-xs">{`    },`}</pre>
+        <pre className="text-xs">{`  },`}</pre>
+        <pre className="text-xs">{`});`}</pre>
+      </CodeBlock>
+
+      <label className="font-semibold">How to use the actions</label>
+      <CodeBlock>
+        <pre className="text-xs text-emphasis-primary">{`// Now instead of a setter, you have an object with the actions`}</pre>
+        <pre className="text-xs text-emphasis-primary">{`// the actions object and keys are memoized and won't change`}</pre>
+        <pre className="text-xs">{`const [count, { increment, decrement }] = useCounterWithActions();`}</pre>
+        <br />
+        <pre className="text-xs text-emphasis-primary">{`// Increase the count by himself each time: 1, 2, 4, 8, 16, ...`}</pre>
+        <pre className="text-xs">
+          {`<Button onClick={`}
+          <span className="text-emphasis-interactive">{'() => increment(count)'}</span>
+          {`}>Increment</Button>`}
+        </pre>
+        <pre className="text-xs">
+          {`<Button onClick={`}
+          <span className="text-emphasis-interactive">{'() => decrement(count)'}</span>
+          {`}>Decrement</Button>`}
+        </pre>
+      </CodeBlock>
+      <ComponentB />
+    </Container>
+  );
+})();
+
 export const GlobalHooksExample = () => {
   return (
     <div className={merge(styles.bgDots, 'text-text-normal', 'flex flex-col gap-4', 'p-4 pb-96 md:px-20')}>
@@ -309,7 +696,7 @@ export const GlobalHooksExample = () => {
 
       <div
         className={merge(
-          'min-h-screen w-full lg:w-3/4 flex flex-col md:grid grid-cols-1 md:grid-cols-2 gap-6',
+          'min-h-screen w-full flex flex-col md:grid grid-cols-1 md:grid-cols-2 gap-6',
           'auto-rows-min',
           'align-middle place-content-start'
         )}
@@ -322,6 +709,13 @@ export const GlobalHooksExample = () => {
 
         <hr className="col-span-2 border-emphasis-secondary border" />
         {reusingSelectorsExample}
+
+        <hr className="col-span-2 border-emphasis-secondary border" />
+        {listeningToStateChanges}
+
+        <hr className="col-span-2 border-emphasis-secondary border" />
+        {moreListeningToStateChanges}
+        {persistStateExample}
       </div>
     </div>
   );
